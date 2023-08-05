c01eeaba2dc2a087f18b88eaf0b6a379019f7119
Fernando Cesar <fernando.csconceicao@outlook.com>
2023-08-05 11:25:30 -0300

    Commit inicial

---
+++ src/main/java/com/example/Reactive/Camel/Mongodb/ReactiveCamelMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Reactive.Camel.Mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCamelMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCamelMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Reactive/Camel/Mongodb/ReactiveCamelMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Reactive.Camel.Mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCamelMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
